b63e4cc15f25887db546eebbe9e69511267029a0
Devashish08 <devashish.cs025@gmail.com>
2024-02-04 02:24:57 +0530

    parse URL and and test cases for it using Jest

---
+++ crawl.test.js
@@ -0,0 +1,32 @@
+/** @format */
+
+const { normalizeURL } = require("./crawl.js");
+const { test, expect } = require("@jest/globals");
+
+test("normalized protocol", () => {
+  const input = "https://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalized slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalized Capitals", () => {
+  const input = "https://BLOG.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL http", () => {
+  const input = "http://BLOG.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
