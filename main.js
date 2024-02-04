5b9369c4bc90001e951e758d5dae8d5bb4eada67
Devashish08 <devashish.cs025@gmail.com>
2024-02-04 16:38:48 +0530

    add crawlPage function

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 /** @format */
+const { crawlPage } = require("./crawl.js");
 
-console.log("Hello World");
+async function main() {
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+  }
+  if (process.argv.length > 3) {
+    console.log("too many arguments provided");
+  }
+
+  const baseURL = process.argv[2];
+
+  console.log(`starting crawl of: ${baseURL}...`);
+
+  await crawlPage(baseURL);
+}
+
+main();
